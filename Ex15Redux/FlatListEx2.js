4d0d0d09d1e91aeb49c6b72c7b3f2062f5e51a20
juhnny <iwibest@naver.com>
2022-07-25 00:16:47 +0900

    update Ex15Redux and init ClassumTest

--- Ex15Redux/FlatListEx2.js
+++ Ex15Redux/FlatListEx2.js
@@ -1,8 +1,10 @@
 import React, { Component, useEffect, useState } from "react";
-import { Animated, Button, FlatList, Image, SafeAreaView, Text, View } from "react-native";
+import { ActivityIndicator, Animated, Button, FlatList, Image, SafeAreaView, Text, View } from "react-native";
 import testSlice, {add, addData, setLoading} from "./testSlice";
 import store from "./store";
 import { useSelector, useDispatch, Provider } from "react-redux";
+import MasonryList from '@react-native-seoul/masonry-list';
+import Spinner from "react-native-loading-spinner-overlay";
 
 const FlatListEx2 = () => {
     return(
@@ -27,7 +29,10 @@ const MyList = () => {
     const loadData = () => {
         dispatch(setLoading(true))
         //fetch를 여기서 했다고 치고
-        newData = [{name: "New", message:"I'm Sam", image: {uri:"https://cdn.pixabay.com/photo/2022/05/29/05/36/otter-7228458_960_720.jpg"}},]
+        newData = [
+            {name: "New", message:"I'm Sam", image: {uri:"https://cdn.pixabay.com/photo/2022/05/29/05/36/otter-7228458_960_720.jpg"}},
+            {name: "New", message:"I'm Sam", image: {uri:"https://cdn.pixabay.com/photo/2022/05/29/05/36/otter-7228458_960_720.jpg"}},
+        ]
         dispatch(addData(newData))
         dispatch(setLoading(false))
     }
@@ -47,7 +52,7 @@ const MyList = () => {
     })
     const dispatch = useDispatch()
 
-    const header = () => {
+    const Header = () => {
         return(
             <View style={{height:40}}>
                 <Text>HEADER</Text>
@@ -55,13 +60,24 @@ const MyList = () => {
         )
     }
 
+    // const [leftColHeight, setLeftColHeight] = useState(0)
+    // const [rightColHeight, setRightColHeight] = useState(0)
+    let leftColHeight = 0
+    let rightColHeight = 0
+
     return(
         //SafeAreaView - iOS에서 노치 영역을 제외한 부분(SafeArea)에만 표현되는 뷰
         <SafeAreaView style={{flex:1, padding:8, backgroundColor:'grey'}}>
             <Text>AAA/{animal}/{slice.data.length}</Text>
-            <FlatList
+            <ActivityIndicator color={'#FF0000'}/>
+            {/* <Spinner
+                visible={true}
+                textContent={'Loading...'}
+                textStyle={{fontWeight:'bold'}}>
+            </Spinner> */}
+            {/* <MasonryList
                 data={slice.data}
-                renderItem={ obj => {
+                renderItem={obj=>{
                     return(
                         <View style={{backgroundColor:'white', borderRadius:8, marginBottom:8}}>
                             <Text style={{height:100}}>{obj.item.name} - {obj.index}</Text>
@@ -72,21 +88,58 @@ const MyList = () => {
                         </View>
                     )
                 }}
-                contentContainerStyle={{backgroundColor:'green'}}
-                ListHeaderComponent={header}
-                stickyHeaderIndices={[0, 2]}
-                // StickyHeaderComponent={header}
+                ListHeaderComponent={Header}
+                // stickyHeaderIndices={[0, 2]}
                 onEndReached={()=> {
                     console.log('End!')
                     if(slice.loading) return
                     else loadData()
                 }}
+                >
+            </MasonryList> */}
+            {/* <FlatList
+                data={slice.data}
+                renderItem={ obj => {
+                    return(
+                        <View
+                            onLayout={event => {
+                                console.log('start', leftColHeight, rightColHeight)
+                                let height = event.nativeEvent.layout.height
+                                console.log(height)
+                                if(obj.index % 2 === 0){
+                                    console.log('left')
+                                    leftColHeight += height
+                                } else {
+                                    console.log('right')
+                                    rightColHeight += height
+                                }    
+                                console.log("end", leftColHeight, rightColHeight)
+                            }} 
+                            style={{ width:"50%", alignSelf:"flex-start", marginTop:0, backgroundColor:'white', borderRadius:8, marginBottom:8}}>
+                            <Text style={{}}>{obj.item.name} - {obj.index}</Text>
+                            <Image 
+                                source={obj.item.image} 
+                                style={{width:"100%", height:150}}></Image>
+                            <Button title="+" onPress={getData}></Button>
+                        </View>
+                    )
+                }}
+                contentContainerStyle={{backgroundColor:'green', alignItems:'baseline'}}
+                ListHeaderComponent={Header}
+                stickyHeaderIndices={[0, 2]}
+                // StickyHeaderComponent={Header}
+                onEndReached={()=> {
+                    console.log('End!')
+                    if(slice.loading) return
+                    else loadData()
+                }}
+                numColumns={2}
                 // onViewableItemsChanged={({viewableItems, changed})=>{
                 //     // console.log("Visible items are: ", viewableItems)
                 //     // console.log("changed in this iteration: ", changed)
                 // }}
                 >
-            </FlatList>
+            </FlatList> */}
         </SafeAreaView>
     )
 }
--- Ex15Redux/testSlice.js
+++ Ex15Redux/testSlice.js
@@ -4,7 +4,7 @@ const testSlice = createSlice({
     name:"testSlice",
     initialState:{
         data:[
-            {name: "Kim0", message:"I'm Sam", image: {uri:"https://firebasestorage.googleapis.com/v0/b/dailydiscovery-9497b.appspot.com/o/uploads%2Fphotos%2FIMG_20220508_055540_.png?alt=media&token=5820cf1d-819e-4cae-a2b8-2bb837608cb0"}},
+            {name: "Kim\n\n\n\n0", message:"I'm Sam", image: {uri:"https://firebasestorage.googleapis.com/v0/b/dailydiscovery-9497b.appspot.com/o/uploads%2Fphotos%2FIMG_20220508_055540_.png?alt=media&token=5820cf1d-819e-4cae-a2b8-2bb837608cb0"}},
             {name: "Kim1", message:"I'm Sam", image: {uri:"https://cdn.pixabay.com/photo/2022/05/29/05/36/otter-7228458_960_720.jpg"}},
             {name: "Kim2", message:"I'm Sam", image: {uri:"https://cdn.pixabay.com/photo/2022/05/29/05/36/otter-7228458_960_720.jpg"}},
             {name: "Kim3", message:"I'm Sam", image: {uri:"https://cdn.pixabay.com/photo/2022/05/29/05/36/otter-7228458_960_720.jpg"}},
